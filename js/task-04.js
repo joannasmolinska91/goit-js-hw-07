25031f3b0c271fe2c2016ca603a2fd66009bb264
Joanna Smolińska-Iciek <joannasmolinska91@gmail.com>
2023-03-09 19:51:11 +0100

    zadanie4

--- js/task-04.js
+++ js/task-04.js
@@ -1,5 +1,35 @@
+const inputEl = document.querySelector("input");
+const createBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-destroy]");
+const boxesContainer = document.querySelector("#boxes");
+
+createBtn.addEventListener("click", () => {
+  const amount = inputEl.value;
+  createBoxes(amount);
+});
+
+destroyBtn.addEventListener("click", () => {
+  destroyBoxes();
+});
+
+function createBoxes(amount) {
+  let boxSize = 30;
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.style.width = `${boxSize}px`;
+    box.style.height = `${boxSize}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesContainer.append(box);
+    boxSize += 10;
+  }
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = "";
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(6, "0")}`;
 }
